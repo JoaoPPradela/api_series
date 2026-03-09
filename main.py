3486210beadeb643d97dd8db8232e0091996ea8b
JoaoPPradela <joao.pradela@aluno.senai.br>
2026-03-09 16:39:37 -0300

    Adicionar testes HTTP para os endpoints soma e adicao

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from api-series!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get('/soma/{a}/{b}')
+def soma(a: int, b: int):
+    return {"resultado": a + b}
