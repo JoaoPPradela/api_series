e6fffcc4b0ffc7f59eb0c838b3dec7cc3c9414b1
JoaoPPradela <joao.pradela@aluno.senai.br>
2026-03-10 14:42:55 -0300

    feat: atualização de dependências e adição de ponto de entrada FastAPI
    - Adicionada dependência de criptografia com versão >=46.0.5 em pyproject.toml e uv.lock.
    
    - Atualizado o arquivo tests.http para usar variáveis ​​de ambiente na URL base e adicionados novos testes de API para criação e recuperação de séries.
    
    - Incluídos os pacotes cffi e pycparser em uv.lock com suas respectivas versões.
    
    - Definido o ponto de entrada FastAPI em pyproject.toml para melhor estrutura da aplicação.

---
+++ app/route/serie.py
@@ -0,0 +1,24 @@
+from pickletools import read_bytes4
+import re
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.model.serie import SerieModel
+from app.schema.serie import SerieSchema
+
+serie = APIRouter()
+
+@serie.post('/')
+async def criar_serie(dados: SerieSchema, db: Session = Depends(get_db)):
+    nova_serie = SerieModel(**dados.model_dump())
+    db.add(nova_serie)
+    db.commit()
+    db.refresh(nova_serie)
+    return nova_serie
+
+@serie.get('/series')
+async def listar_series(db: Session = Depends(get_db)):
+    return db.query(SerieModel).all()
+
+@serie.get('/atualizar')
